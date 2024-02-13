af4a98d56a714b546f026a2bd6e6a703496857fd
Verônica Vilas Boas <135287830+VeronicaVilas@users.noreply.github.com>
2024-02-13 13:49:15 -0300

    feat: adicionado exercicio semana 05

---
+++ Semana05/routes/reviews.php
@@ -0,0 +1,16 @@
+<?php
+require_once '../config.php';
+require_once '../controller/ReviewController.php';
+
+$method = $_SERVER['REQUEST_METHOD'];
+$controller = new ReviewController();
+
+if ($method === 'POST') {
+    $controller->create();
+} else if ($method === 'GET' && !isset($_GET['id'])) {
+    $controller->list();
+} else if ($method === 'GET' && $_GET['id']) {
+    $controller->listOne();
+} else if ($method === 'PUT') {
+   $controller->update();
+}
